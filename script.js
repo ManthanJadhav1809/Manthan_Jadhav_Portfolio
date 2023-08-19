7efffb2d684bdb509664d3a063fac4c68732e80e
Manthan Jadhav <129407291+ManthanJadhav1809@users.noreply.github.com>
2023-08-19 17:45:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,21 +30,25 @@ function addReposToCard(repos) {
     });
 }
 
-const header = document.getElementById('header');
+const header = document.querySelector('.header');
 const footer = document.getElementById('footer');
 const splashImg = document.getElementById('splashImg')
 const maincode = document.getElementById('maincode')
-
+const myBtn = document.getElementById('myBtn')
+const contact = document.getElementById('contact')
 
 maincode.style.display = 'none'
 header.style.display = 'none'
 footer.style.display = 'none'
+myBtn.style.display = 'none'
+contact.style.display = 'none'
 
 setTimeout(() => {
-    splashImg.style.display = 'none';
+     splashImg.style.display = 'none';
     maincode.style.display = 'block';
     header.style.display = 'flex';
     footer.style.display = 'block';
+    myBtn.style.display = 'block'
 
 }, 3500)
 
@@ -52,20 +56,16 @@ setTimeout(() => {
 
 // Sticky scrollbar
 
-function getYPosition() {
-    var top = window.pageYOffset || document.documentElement.scrollTop
-    return top;
-  }
+ header.classList.toggle('sticky',window.scrollY>100);
 
-document.addEventListener('scroll', () => {
-    var scroll = getYPosition()
-    if (scroll < 150) {
-        header.classList.remove("sticky");
-    } else {
-        header.classList.add("sticky");
-    }
-
-})
+ let menuIcon = document.querySelector('#menu-icon');
+ let navbar = document.querySelector('.Navbar');
+ 
+ menuIcon.onclick=()=>{
+  // alert("hh");
+  menuIcon.classList.toggle('fa-x');
+  navbar.classList.toggle('active');
+ }
 
 // Get the button
 let mybutton = document.getElementById("myBtn");
@@ -86,3 +86,30 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Save contact details to google sheet
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyTbQ6L7ikLv2CZO9acGbpUGv0LO6QXISoHnaFW1VBnittRAKO42bmMdw_Fj_NCt4IX/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML="Message sent Sucessfully"
+        setTimeout(()=>{
+            msg.innerHTML=""
+        },4000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+// text
+const typed=new Typed('.multiple-text',{
+  strings:['Frontend Developer','Web Developer'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
